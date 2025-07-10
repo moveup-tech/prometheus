dded019a553b978580c657b4cb9de00bd3b69c38
daniellvaz <daniellmvaz@gmail.com>
2025-07-10 14:42:15 -0300

    Updates

---
+++ assets/button.stories-eGXxokYW.js
@@ -0,0 +1,53 @@
+import{j as q}from"./jsx-runtime-z8MfsBtr.js";import{B as i}from"./index-M-X4Dozj.js";import{c as G}from"./createLucideIcon-BT4tydPZ.js";import"./index-C9rmetsa.js";import"./index-CQJnhAbd.js";import"./index-BEn-qc9j.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const F=G("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const H=G("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]),Q={render:({children:M,...b})=>q.jsx(i,{...b,children:M}),component:i,title:"Forms/Button",tags:["autodocs"]},r={args:{variant:"default",children:"Button"}},e={args:{variant:"secondary",children:"Button"}},t={args:{variant:"destructive",children:"Button"}},a={args:{variant:"ghost",children:"Button"}},n={args:{variant:"link",children:"Button",as:"a",href:"#"}},o={args:{children:"Button",iconLeft:F}},s={args:{children:"Button",iconRight:H}},c={args:{children:"Button",isLoading:!0}};var d,u,m;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: "default",
+    children: "Button"
+  }
+}`,...(m=(u=r.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var p,g,l;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Button"
+  }
+}`,...(l=(g=e.parameters)==null?void 0:g.docs)==null?void 0:l.source}}};var h,B,f;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    variant: "destructive",
+    children: "Button"
+  }
+}`,...(f=(B=t.parameters)==null?void 0:B.docs)==null?void 0:f.source}}};var v,L,S;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: "ghost",
+    children: "Button"
+  }
+}`,...(S=(L=a.parameters)==null?void 0:L.docs)==null?void 0:S.source}}};var y,k,x;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    variant: "link",
+    children: "Button",
+    as: "a",
+    href: "#"
+  }
+}`,...(x=(k=n.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};var R,w,A;o.parameters={...o.parameters,docs:{...(R=o.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    iconLeft: ArrowLeft
+  }
+}`,...(A=(w=o.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var I,D,j;s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    iconRight: ArrowRight
+  }
+}`,...(j=(D=s.parameters)==null?void 0:D.docs)==null?void 0:j.source}}};var _,z,E;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    isLoading: true
+  }
+}`,...(E=(z=c.parameters)==null?void 0:z.docs)==null?void 0:E.source}}};const T=["Default","Secondary","Destructive","Ghost","Link","IconLeft","IconRight","Sloading"];export{r as Default,t as Destructive,a as Ghost,o as IconLeft,s as IconRight,n as Link,e as Secondary,c as Sloading,T as __namedExportsOrder,Q as default};
